a1b98cfcd901f0dbe43900e8468ec15d5615066b
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2021-09-15 19:52:28 -0300

    Finalizado

--- tareas.js
+++ tareas.js
@@ -1,34 +1,38 @@
 const fs = require('fs'); // Requerir el File System
 
-module.exports = moduloTareas = { 
-    leerJSON : () => {
-        const listaDeTareas = fs.readFileSync('./tareas.json', 'utf-8'); 
+module.exports = moduloTareas = {
+    leerJSON: () => {
+        const listaDeTareas = fs.readFileSync('./tareas.json', 'utf-8');
         return JSON.parse(listaDeTareas)
     },
-    escribirJSON: (titulo, estado) => {   
-         const nuevaTarea = {
-             titulo : titulo,
-             estado : "pendiente"
-         }
-         const tareasAnteriores = moduloTareas.leerJSON();  
-         tareasAnteriores.push(nuevaTarea);   
-         moduloTareas.guardarTarea(tareasAnteriores);  
+
+    escribirJSON: (titulo, estado) => {
+        const nuevaTarea = {
+            titulo: titulo,
+            estado: "pendiente"
+        }
+        const tareasAnteriores = moduloTareas.leerJSON();
+        tareasAnteriores.push(nuevaTarea);
+        moduloTareas.guardarTarea(tareasAnteriores);
     },
+
     guardarTarea: (info) => {
         const nuevoJSON = JSON.stringify(info);
-        fs.writeFileSync('./tareas.json', nuevoJSON, 'utf-8' );  
+        fs.writeFileSync('./tareas.json', nuevoJSON, 'utf-8');
     },
+
     deshacer: () => {
         const tareas = moduloTareas.leerJSON();
         tareas.pop();   // Elimina el ultimo
         moduloTareas.guardarTarea(tareas);
     },
+
     filtrarPorEstado: (estados) => {
         const listaDeTareas = moduloTareas.leerJSON();
         const tareasFiltradas = listaDeTareas.filter(tarea => {
             return tarea.estado.toLowerCase() === estados.toLowerCase();
-        })  
+        })
         return tareasFiltradas
     }
-} 
+}
 //console.log(moduloTareas.filtrarPorEstado("terminado"))
